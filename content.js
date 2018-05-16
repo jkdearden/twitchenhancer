ba43fc8bf1eff33e2d3d4e946ae2c52fe761c147
Joseph Dearden <jkdearden@gmail.com>
2018-05-16 15:41:26 -0700

    cleaner, better-functioning implementation with mutationobserver

--- content.js
+++ content.js
@@ -1,28 +1,35 @@
-function liveLinkReplacer() {
+window.addEventListener("load", main, false);
+
+function linkReplacer() {
 	let cards = document.querySelectorAll("div.live-channel-card");
 	cards.forEach(card => {
-		var thumbLink = card.querySelector("div > div > div:nth-child(1) > figure > div > a:nth-child(1)");
-		var thumbFig = card.querySelector("div > div > div:nth-child(1) > figure");
-
-		var prevRef = thumbLink.href;
-		var newRef = prevRef.replace("www.twitch.tv/", "player.twitch.tv/?channel=");
-		thumbLink.setAttribute('href', newRef);
-		thumbFig.addEventListener('click', () => location.assign(newRef));
+		replaceLink(card);
 	});
 }
 
-function hostedLinkReplacer() {
+function replaceLink(node) {
+	var thumbLink = node.querySelector("div > div > div:nth-child(1) > figure > div > a:nth-child(1)");
+	var thumbFig = node.querySelector("div > div > div:nth-child(1) > figure");
 
+	var prevRef = thumbLink.href;
+	var newRef = prevRef.replace("www.twitch.tv/", "player.twitch.tv/?channel=");
+	thumbLink.setAttribute("href", newRef);
+	thumbFig.addEventListener("click", () => location.assign(newRef));
 }
 
-window.addEventListener("load", main, false);
+
+function domChangeCallback(mutationsList) {
+	mutationsList.forEach(mut => {
+		if (mut.addedNodes.length > 0) {
+			console.log("replacing");
+			linkReplacer();
+		}
+	})
+}
 
 function main(evt) {
-	var jsInitChecktimer = setInterval (checkForJS_Finish, 111);
-    function checkForJS_Finish () {
-        if (document.querySelector("div.live-channel-card")) {
-            clearInterval (jsInitChecktimer);
-			liveLinkReplacer()
-        }
-    }
+	var targetNode = document.querySelector(".twilight-main");
+	var config = {subtree: true, childList: true}
+	var observer = new MutationObserver(domChangeCallback);
+	observer.observe(targetNode, config);
 }
