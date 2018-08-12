2e73085f9fe0c3e74c61795418062aafcff1e788
Joseph Dearden <jkdearden@gmail.com>
2018-08-12 09:12:47 -0700

    add sidebar link redirect fix directory page redirects

--- content.js
+++ content.js
@@ -1,13 +1,24 @@
 window.addEventListener("load", main, false);
 
 function cardLinkReplacer() {
-	let cards = document.querySelectorAll("div.preview-card");
-	cards.forEach(card => {
-		replaceLink(card);
-	});
+	if (document.URL.includes("following")) {
+		let followingCards = document.querySelectorAll("div.tw-pd-3 > div:nth-child(2) div.preview-card");
+		followingCards.forEach(card => { replaceFollowing(card); })
+
+		let hostingCards = document.querySelectorAll("div.tw-pd-3 > div:nth-child(4) div.preview-card");
+		hostingCards.forEach(card => { replaceHosting(card); })		
+	}
+	else {
+		let cards = document.querySelectorAll("div.preview-card")
+		cards.forEach(card => { replaceFollowing(card); })
+	}
+
+
+	let sidenavCards = document.querySelectorAll("div.side-nav-card")
+	sidenavCards.forEach(card => { replaceSidenav(card); })
 }
 
-function replaceLink(node) {
+function replaceFollowing(node) {
 	var thumbLink = node.querySelector("div:nth-child(1) > a");
 	var thumbFig = node.querySelector("div:nth-child(2) > div:nth-child(2) > div > a");
 
@@ -19,11 +30,31 @@ function replaceLink(node) {
 	thumbFig.addEventListener("click", () => location.assign(newRef));
 }
 
+function replaceHosting(node) {
+	var thumbLink = node.querySelector("div:nth-child(1) > a");
+	var thumbFig = node.querySelector("div:nth-child(2) > div:nth-child(2) > div > a");
+	var hosterName = node.querySelector("div.preview-card-titles__subtitle-wrapper > div:nth-child(1) > p > a").textContent
+
+	var newRef = "https://player.twitch.tv/?channel=" + hosterName
+	thumbLink.setAttribute("href", newRef);
+	thumbLink.addEventListener("click", () => location.assign(newRef));
+	thumbFig.setAttribute("href", newRef);
+	thumbFig.addEventListener("click", () => location.assign(newRef));
+}
+
+function replaceSidenav(node) {
+	var link = node.querySelector("a.side-nav-card__link");
+
+	if (!link.textContent.includes("Offline")) {
+		var prevRef = link.href;
+		var newRef = prevRef.replace("www.twitch.tv/", "player.twitch.tv/?channel=");
+		link.setAttribute("href", newRef);
+	}
+}
 
 function domChangeCallback(mutationsList) {
 	mutationsList.forEach(mut => {
 		if (mut.addedNodes.length > 0) {
-			console.log("replacing");
 			cardLinkReplacer();
 		}
 	})
