1a73d9a71823187dfe539edb266a1b0f3a7716b3
Joseph Dearden <jkdearden@gmail.com>
2019-04-15 12:47:41 -0700

    fixes
    whitelist more urls from redirecting
    delete last tab after redirect so back button works (if you hit it twice >_>)

--- content.js
+++ content.js
@@ -4,8 +4,12 @@ chrome.tabs.onUpdated.addListener(
 		if (changeInfo.url) {
 			var prevUrl = changeInfo.url
 			var matches = prevUrl.match("http[s?]://(www.)?twitch.tv/([^/]*)$")
-			if (matches && !matches.includes("directory") && !matches.includes("videos")) {
-				chrome.tabs.update(tabId, {url: prevUrl.replace("www.", "").replace("twitch.tv/", "player.twitch.tv/?channel=")})
+			if (matches && !matches.includes("directory") && !matches.includes("videos")
+				&& !matches.includes("friends") && !matches.includes("inventory")
+				&& !matches.includes("subscriptions") && !matches.includes("payments")
+				&& !matches.includes("settings")) {
+					chrome.history.deleteUrl({"url": prevUrl})
+					chrome.tabs.update(tabId, {url: prevUrl.replace("www.", "").replace("twitch.tv/", "player.twitch.tv/?channel=")})
 			}
 		}
 	}
